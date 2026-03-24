7c5001f8ffa0657f62112a5d87c1ad82d814b4e0
gschumacher87-art <gschumacher87@gmail.com>
2026-03-24 21:17:03 +1100

    Update food.js

--- js/food.js
+++ js/food.js
@@ -4,21 +4,27 @@ export class Food {
         this.grid = grid;
         this.canvasWidth = canvasWidth;
         this.canvasHeight = canvasHeight;
-        this.position = this.randomPosition();
+        this.position = { x: 0, y: 0 };
     }
 
-    randomPosition() {
+    randomPosition(snakeBody = []) {
         const tilesX = Math.floor(this.canvasWidth / this.grid);
         const tilesY = Math.floor(this.canvasHeight / this.grid);
-        this.position = {
-            x: Math.floor(Math.random()*tilesX)*this.grid,
-            y: Math.floor(Math.random()*tilesY)*this.grid
-        };
-        return this.position;
+
+        let newPos;
+
+        do {
+            newPos = {
+                x: Math.floor(Math.random() * tilesX) * this.grid,
+                y: Math.floor(Math.random() * tilesY) * this.grid
+            };
+        } while (snakeBody.some(seg => seg.x === newPos.x && seg.y === newPos.y));
+
+        this.position = newPos;
     }
 
     draw(ctx) {
         ctx.fillStyle = "red";
-        ctx.fillRect(this.position.x, this.position.y, this.grid-2, this.grid-2);
+        ctx.fillRect(this.position.x, this.position.y, this.grid - 2, this.grid - 2);
     }
 }
