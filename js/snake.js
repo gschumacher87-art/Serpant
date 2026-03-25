034f91dfe1bd5ad1eeddd29685a0d8baa4a913ff
gschumacher87-art <gschumacher87@gmail.com>
2026-03-25 16:25:56 +1100

    Update snake.js

--- js/snake.js
+++ js/snake.js
@@ -85,73 +85,117 @@ export class Snake {
 
     getHead() { return this.body[0]; }
 
-    // ===== DRAW METHOD (LIME SNAKE) =====
     draw(ctx) {
-        const head = this.body[0];
-        const hx = head.x + this.grid / 2;
-        const hy = head.y + this.grid / 2;
+    const head = this.body[0];
+    const hx = head.x + this.grid / 2;
+    const hy = head.y + this.grid / 2;
 
-        // 1. ROTATION BASED ON DIRECTION
-        const angle = Math.atan2(this.dy, this.dx);
-        ctx.save();
-        ctx.translate(hx, hy);
-        ctx.rotate(angle);
+    // 1. ROTATION BASED ON DIRECTION
+    const angle = Math.atan2(this.dy, this.dx);
+    ctx.save();
+    ctx.translate(hx, hy);
+    ctx.rotate(angle);
 
-        // 2. HEAD (simple lime oval)
-        ctx.fillStyle = "lime";
+    const sizeX = this.grid * 0.6;
+    const sizeY = this.grid * 0.4;
+
+    // 2. HEAD (gradient for realism)
+    const headGradient = ctx.createRadialGradient(0, 0, sizeY * 0.3, 0, 0, sizeX);
+    headGradient.addColorStop(0, "#A4F54C"); // lighter center
+    headGradient.addColorStop(1, "#2E7D32"); // darker edges
+    ctx.fillStyle = headGradient;
+    ctx.beginPath();
+    ctx.ellipse(0, 0, sizeX, sizeY, 0, 0, Math.PI * 2);
+    ctx.fill();
+    ctx.strokeStyle = "#1B5E20";
+    ctx.lineWidth = 2;
+    ctx.stroke();
+
+    // 3. SCALES (subtle texture)
+    ctx.strokeStyle = "rgba(0,0,0,0.1)";
+    ctx.lineWidth = 0.5;
+    for (let i = -sizeX + 2; i < sizeX; i += 4) {
         ctx.beginPath();
-        ctx.ellipse(0, 0, this.grid * 0.6, this.grid * 0.4, 0, 0, Math.PI * 2);
+        ctx.moveTo(i, -sizeY);
+        ctx.lineTo(i + 2, sizeY);
+        ctx.stroke();
+    }
+
+    // 4. EYES (realistic with iris + highlight)
+    const eyeOffsetX = sizeX * 0.3;
+    const eyeOffsetY = sizeY * 0.1;
+    const eyeRadius = sizeY * 0.15;
+
+    if (!this.blink) {
+        // Eye white
+        ctx.fillStyle = "#FFFFFF";
+        ctx.beginPath();
+        ctx.ellipse(eyeOffsetX, -eyeOffsetY, eyeRadius, eyeRadius * 0.6, 0, 0, Math.PI * 2);
         ctx.fill();
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 2;
-        ctx.stroke();
 
-        // 3. EYES (black)
-        if (!this.blink) {
-            ctx.fillStyle = "black";
-            ctx.beginPath();
-            ctx.ellipse(this.grid * 0.2, -this.grid * 0.1, this.grid * 0.05, this.grid * 0.1, 0, 0, Math.PI * 2);
-            ctx.ellipse(this.grid * 0.2, this.grid * 0.1, this.grid * 0.05, this.grid * 0.1, 0, 0, Math.PI * 2);
-            ctx.fill();
-        }
-
-        // 4. MOUTH
         ctx.beginPath();
-        if (this.mouthOpen) ctx.moveTo(-this.grid * 0.2, 0), ctx.lineTo(this.grid * 0.3, 0.2);
-        else ctx.moveTo(-this.grid * 0.2, 0), ctx.lineTo(this.grid * 0.3, 0);
-        ctx.strokeStyle = "black";
-        ctx.lineWidth = 1.5;
+        ctx.ellipse(eyeOffsetX, eyeOffsetY, eyeRadius, eyeRadius * 0.6, 0, 0, Math.PI * 2);
+        ctx.fill();
+
+        // Iris
+        ctx.fillStyle = "#1A1A00";
+        ctx.beginPath();
+        ctx.ellipse(eyeOffsetX, -eyeOffsetY, eyeRadius * 0.5, eyeRadius * 0.3, 0, 0, Math.PI * 2);
+        ctx.fill();
+
+        ctx.beginPath();
+        ctx.ellipse(eyeOffsetX, eyeOffsetY, eyeRadius * 0.5, eyeRadius * 0.3, 0, 0, Math.PI * 2);
+        ctx.fill();
+
+        // Highlight
+        ctx.fillStyle = "rgba(255,255,255,0.7)";
+        ctx.beginPath();
+        ctx.arc(eyeOffsetX - eyeRadius * 0.2, -eyeOffsetY - eyeRadius * 0.1, eyeRadius * 0.15, 0, Math.PI * 2);
+        ctx.fill();
+        ctx.beginPath();
+        ctx.arc(eyeOffsetX - eyeRadius * 0.2, eyeOffsetY - eyeRadius * 0.1, eyeRadius * 0.15, 0, Math.PI * 2);
+        ctx.fill();
+    }
+
+    // 5. MOUTH (curved, can open)
+    ctx.strokeStyle = "#1B5E20";
+    ctx.lineWidth = 1.5;
+    ctx.beginPath();
+    if (this.mouthOpen) {
+        ctx.moveTo(-sizeX * 0.3, sizeY * 0.2);
+        ctx.quadraticCurveTo(sizeX * 0.2, sizeY * 0.4, sizeX * 0.4, 0);
+    } else {
+        ctx.moveTo(-sizeX * 0.3, sizeY * 0.1);
+        ctx.quadraticCurveTo(sizeX * 0.2, sizeY * 0.2, sizeX * 0.4, 0);
+    }
+    ctx.stroke();
+
+    // 6. TONGUE (forked, animated)
+    if (this.tongueOut) {
+        ctx.strokeStyle = "#FF0000";
+        ctx.lineWidth = 1.8;
+        ctx.beginPath();
+        ctx.moveTo(sizeX * 0.4, 0);
+        ctx.lineTo(sizeX * 0.6, -sizeY * 0.15);
+        ctx.moveTo(sizeX * 0.4, 0);
+        ctx.lineTo(sizeX * 0.6, sizeY * 0.15);
         ctx.stroke();
+    }
 
-        // 5. TONGUE
-        if (this.tongueOut) {
-            ctx.beginPath();
-            ctx.moveTo(this.grid * 0.3, 0);
-            ctx.lineTo(this.grid * 0.5, 0);
-            ctx.moveTo(this.grid * 0.5, 0);
-            ctx.lineTo(this.grid * 0.55, -this.grid * 0.1);
-            ctx.moveTo(this.grid * 0.5, 0);
-            ctx.lineTo(this.grid * 0.55, this.grid * 0.1);
-            ctx.strokeStyle = "red";
-            ctx.lineWidth = 2;
-            ctx.stroke();
-        }
+    ctx.restore();
 
-        ctx.restore();
+    // 7. BODY (same as before)
+    for (let i = 1; i < this.body.length; i++) {
+        const seg = this.body[i];
+        const prev = this.body[i - 1];
 
-        // 6. BODY (all lime)
-        for (let i = 1; i < this.body.length; i++) {
-            const seg = this.body[i];
-            const prev = this.body[i - 1];
+        ctx.beginPath();
+        ctx.lineCap = "round";
+        ctx.lineWidth = this.grid * (1 - i / this.body.length * 0.5);
+        ctx.strokeStyle = "lime";
 
-            ctx.beginPath();
-            ctx.lineCap = "round";
-            ctx.lineWidth = this.grid * (1 - i / this.body.length * 0.5);
-            ctx.strokeStyle = "lime";
-
-            ctx.moveTo(prev.x + this.grid / 2, prev.y + this.grid / 2);
-            ctx.lineTo(seg.x + this.grid / 2, seg.y + this.grid / 2);
-            ctx.stroke();
-        }
+        ctx.moveTo(prev.x + this.grid / 2, prev.y + this.grid / 2);
+        ctx.lineTo(seg.x + this.grid / 2, seg.y + this.grid / 2);
+        ctx.stroke();
     }
 }
